9ed4e6fac2e9aff5a580b671bc259223a3735b9b
anugrahp13 <anugrahprastyo13@gmail.com>
2024-07-12 23:04:58 +0700

    Build [Layout.jsx]: create file

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,25 @@
+import { PropTypes } from "prop-types";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/header/Header";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col ">
+      <div className="flex flex-col justify-between min-h-screen">
+        <div>
+          <header className="bg-white border-b py-5 sticky top-0">
+            <Navbar />
+          </header>
+          <main className="py-7">{children}</main>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
