fedb516264db6096fe0b72134752b78d1231255f
anugrahp13 <anugrahprastyo13@gmail.com>
2024-07-13 01:33:24 +0700

    refactor [*]

--- src/pages/CheckoutHistory.jsx
+++ src/pages/CheckoutHistory.jsx
@@ -1,24 +1,26 @@
 import { Link } from "react-router-dom";
 import useStore from "../stores/store";
 import BelumBelanja from "./BelumBelanja";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/ui/Button";
 
 export const CheckoutHistory = () => {
   const store = useStore();
   const history = store.history;
-
+  const navigate = useNavigate();
   return (
     <>
-      <div className="max-w-3xl mx-auto px-4">
+      <div className="max-w-3xl mx-auto px-4 grid gap-2">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold mb-4">Checkout History</h2>
-          <p
+          <Button
             onClick={store.clearHistory}
-            className="text-green-600 hover:text-red-600 cursor-pointer transition-all duration-500"
+            style="text-base font-normal hover:bg-green-600 text-slate-800 hover:text-white"
           >
             Clear History
-          </p>
+          </Button>
         </div>
-        <div className="border p-4 h-[60vh] overflow-y-auto">
+        <div className="border rounded-xl p-4 h-[60vh] overflow-y-auto">
           {store.history.length === 0 ? (
             <div className="flex h-full items-center justify-center">
               <BelumBelanja />
@@ -27,14 +29,14 @@ export const CheckoutHistory = () => {
             <div className="flex flex-col gap-1">
               {history.map((checkout) => (
                 <Link to={`/history/${checkout.id}`} key={checkout.id}>
-                  <div className="flex justify-between items-center border shadow-sm p-4 rounded-md hover:shadow-green-200 transition-all duration-500">
+                  <div className="flex justify-between items-center border shadow-sm p-4 rounded-lg hover:shadow-green-200 transition-all duration-500">
                     <div className="flex flex-col gap-1">
-                      <p className="text-xs text-gray-500">Tanggal Belanja</p>
+                      <p className="text-xs text-slate-800">Tanggal Belanja</p>
                       <p className="text-sm">{checkout.date}</p>
                     </div>
                     <div className="flex flex-col gap-1">
-                      <p className="text-xs text-gray-500">Total belanja: </p>
-                      <p className="text-sm font-semibold">{checkout.totalPrice}</p>
+                      <p className="text-xs text-gray-500">Total belanja</p>
+                      <p className="text-sm font-semibold">$ {checkout.totalPrice}</p>
                     </div>
                   </div>
                 </Link>
@@ -42,11 +44,9 @@ export const CheckoutHistory = () => {
             </div>
           )}
         </div>
-        <Link to="/">
-          <p className="text-end my-4 text-green-600 cursor-pointer hover:text-green-300 transition-all duration-500">
+        <Button onClick={() => navigate("/")} style="text-base bg-green-600 text-white">
             Belanja Lagi!
-          </p>
-        </Link>
+        </Button>
       </div>
     </>
   );
